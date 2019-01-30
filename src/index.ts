59760aaf45b7a6b69d3902df638ffa92945c802f
canguruhh <sven@mutzl.org>
2019-01-30 23:44:46 +0000

    Some comments

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,26 @@ const SYNC_INTERVAL = 1000
 const RETRY_INTERVAL = 5000
 const URL = 'mongodb://localhost:27017/mvs'
 
+//Set your handlers here
 let BlockHandlers = [
     new DifficultyPoW(),
     new DifficultyPoS()
 ]
-let last: number = 0
 
-
-async function calculateInterval() {
+async function calculateInterval(last: number) {
     console.log(`try with interval starting from ${last}`)
     let endblock = await BlockModel.findOne({ number: last + SYNC_INTERVAL + SYNC_THRESHOLD, orphan: 0 })
     if (!endblock) {
         return
     }
     let interval = await BlockModel.find({ number: { $lt: last + SYNC_INTERVAL, $gte: last }, orphan: 0 })
-    let result = await Promise.all(BlockHandlers.map(handler => handler.calculate(interval)))
-    result.forEach(async (datapoint)=>{
-        if(datapoint)
-            await StatisticModel.update({type: datapoint.type, height: datapoint.height}, datapoint, {upsert: true, setDefaultsOnInsert: true});
+    let datapoints = await Promise.all(BlockHandlers.map(handler => handler.calculate(interval)))
+    datapoints.forEach(async (datapoint) => {
+        if (datapoint)
+            await StatisticModel.update({ type: datapoint.type, height: datapoint.height }, datapoint, { upsert: true, setDefaultsOnInsert: true });
     })
     last += SYNC_INTERVAL
-    return result
+    return datapoints
 }
 
 function sleep(millis: number) {
@@ -38,13 +37,13 @@ function sleep(millis: number) {
     })
 }
 
+// Main processing loop
 (async () => {
+    let last: number = 0
     let db: Mongoose = await connect(URL, { useNewUrlParser: true });
     while (true) {
-        let result = await calculateInterval()
-        if (result)
-            console.log(result)
-        else
+        let result = await calculateInterval(last)
+        if (!result)
             await sleep(RETRY_INTERVAL)
     }
 })()
